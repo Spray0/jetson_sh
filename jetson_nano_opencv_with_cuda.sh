a6bf1a726cb9855a8f5117cf92ce7b27d4f68baf
Chenjingyun <javen.chan@outlook.com>
2023-11-14 19:41:59 +0800

    Update jetson_nano_opencv_with_cuda.sh

--- jetson_nano_opencv_with_cuda.sh
+++ jetson_nano_opencv_with_cuda.sh
@@ -3,10 +3,17 @@ echo hello jetson nano!
 
 sudo add-apt-repository "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ xenial main multiverse restricted universe"
 sudo apt update
-sudo apt install libjasper1 libjasper-dev
-sudo apt install -y build-essential checkinstall cmake pkg-config yasm git gfortran
-sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libjasper1 libjasper-dev
+sudo apt install -y build-essential checkinstall cmake pkg-config yasm git gfortran uzip
 sudo apt install -y libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev
 sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils
-sudo apt install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+sudo apt install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
+cd ~/Downloads
+wget -O opencv.zip https://github.com/opencv/opencv/archive/refs/tags/4.7.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/refs/tags/4.7.0.zip
+unzip opencv.zip
+unzip opencv_contrib.zip
+cd opencv-4.7.0
+mkdir build
+cd build
