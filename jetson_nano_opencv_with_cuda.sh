0d4b2efe1f4d7746ebfef48061f481c2a48c776e
Chenjingyun <javen.chan@outlook.com>
2023-11-14 19:28:54 +0800

    Update jetson_nano_opencv_with_cuda.sh

--- jetson_nano_opencv_with_cuda.sh
+++ jetson_nano_opencv_with_cuda.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 echo hello jetson nano!
-#sudo apt-get install python3-pip
-#sudo pip3 install jetson-stats
 
-sudo apt purge libopencv*
-sudo apt autoremove
+sudo apt update
+sudo apt install -y build-essential checkinstall cmake pkg-config yasm git gfortran
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgtk2.0-dev libtbb-dev libatlas-base-dev libfaac-dev libmp3lame-dev libtheora-dev libvorbis-dev libxvidcore-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils
+sudo apt install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
