52c513cffc44607a92cb0869d44627c6db53e580
Chenjingyun <javen.chan@outlook.com>
2023-11-14 19:31:28 +0800

    Update jetson_nano_opencv_with_cuda.sh

--- jetson_nano_opencv_with_cuda.sh
+++ jetson_nano_opencv_with_cuda.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo hello jetson nano!
 
+sudo add-apt-repository "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu-ports/ xenial main multiverse restricted universe"
 sudo apt update
+sudo apt install libjasper1 libjasper-dev
 sudo apt install -y build-essential checkinstall cmake pkg-config yasm git gfortran
 sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt install -y libjpeg8-dev libjasper-dev libpng12-dev libtiff5-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev
