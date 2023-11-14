268545a921acf41bb984b9f673301289ce840b38
Chenjingyun <javen.chan@outlook.com>
2023-11-14 19:22:32 +0800

    Add files via upload

---
+++ jetson_nano_jtop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo hello jetson nano!
+sudo apt-get install python3-pip
+sudo pip3 install jetson-stats
---
+++ jetson_nano_remove_opencv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo hello jetson nano!
+sudo apt purge libopencv*
+sudo apt autoremove
